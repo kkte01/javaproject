c951d3a6bffc2593976b5db6747e5cadfb365901
kkte02 <kkte02@naver.com>
2020-10-26 15:07:40 +0900

    DB password 수정

--- src/controller/DBUtil.java
+++ src/controller/DBUtil.java
@@ -10,7 +10,7 @@ public class DBUtil {
    public static final String URL = "jdbc:mariadb://jbstv.synology.me:3307/poketmon";
    public static Connection getConnection() throws Exception {
       Class.forName(DRIVER);
-      Connection con = DriverManager.getConnection(URL,"poketmon","Pirates!11");
+      Connection con = DriverManager.getConnection(URL,"pocketmon","Class61234!");
       //üũ
       if(con != null) {
     	  
